c86577baeca2fcd9dda3ed943545fba4db0f46e2
mundo88 <minhphung8898@gmail.com>
2022-08-16 04:46:10 +0700

    first commit

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,46 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/modunnn',
+    icon: 'twitter',
+  },
+  {
+    label: 'instagram',
+    url: 'https://www.instagram.com/p____.minh/',
+    icon: 'instagram',
+  }, {
+    label: 'facebook',
+    url: 'https://www.facebook.com/p.v.minh8/',
+    icon: 'facebook',
+  },
+  // {
+  //   label: 'Figma',
+  //   url: 'https://www.figma.com/@phungminh',
+  //   icon: 'figma',
+  // },
+  {
+    label: 'Github',
+    url: 'https://github.com/mundo88',
+    icon: 'github',
+  },
+
+];
