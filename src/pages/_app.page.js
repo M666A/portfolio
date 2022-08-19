8fdf72bc534b57fd67b94f9accb47983fde9236d
mundo88 <minhphung8898@gmail.com>
2022-08-20 02:01:09 +0700

    ok

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -21,7 +21,7 @@ export const AppContext = createContext({});
 const repoPrompt = `
 __  __  __
 \u005C \u005C \u005C \u005C \u005C\u2215\n \u005C \u005C\u2215\u005C \u005C\n  \u005C\u2215  \u005C\u2215
-\n\nTaking a peek huh? Check out the source code: https://github.com/mundo88/Portfolio
+\n\nTaking a peek huh? Check out the source code: https://github.com/dinhquangminh98/portfolio
 `;
 
 const App = ({ Component, pageProps }) => {
